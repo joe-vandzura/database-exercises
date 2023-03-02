79d3291d85ab0ddc5fc9d5e6629275e2061dd455
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-02 10:47:05 -0600

    finished part two of where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,13 @@
 use ymir_employees;
 
-select *
-from employees
-where first_name
-in ('Irena', 'Vidya', 'Maya');
+select * from employees where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
 
 select * from employees where last_name like 'E%';
 
 select * from employees where last_name like '%q%';
+
+select * from employees where (last_name like 'E%') or (last_name like '%e');
+
+select * from employees where (last_name like 'E%') and (last_name like '%e');
+
+select * from employees where (last_name like '%q%') and (not last_name like '%qu%');;
