5714c18e9dcb863ba7b62a049a33b9b7f4ff0621
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-03 15:09:00 -0600

    finished bonus part of employee database exercise of joins exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,4 +1,5 @@
-select d.dept_name as department_name, CONCAT(e.first_name, ' ', e.last_name) as department_manager
+select d.dept_name as 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) as 'Department Manager'
 from departments as d
     inner join dept_manager as dm
 on d.dept_no = dm.dept_no
@@ -6,7 +7,9 @@ on d.dept_no = dm.dept_no
         on dm.emp_no = e.emp_no
 where dm.to_date = '9999-01-01';
 
-select d.dept_name as department_name, concat(e.first_name, ' ', e.last_name) as department_manager
+
+select d.dept_name as 'Department Name',
+       concat(e.first_name, ' ', e.last_name) as 'Department Manager'
 from departments as d
     inner join dept_manager as dm
 on d.dept_no = dm.dept_no
@@ -14,17 +17,9 @@ on d.dept_no = dm.dept_no
 on dm.emp_no = e.emp_no
 where e.gender = 'F' and dm.to_date = '9999-01-01';
 
-select t.title, count(*) as total
-from titles as t
-inner join employees as e
-    on t.emp_no = e.emp_no
-inner join dept_emp as de
-    on e.emp_no = de.emp_no
-where de.dept_no = 'd009' and de.to_date = '9999-01-01'
-group by title;
 
-select t.title as 'title',
-       COUNT(*) as 'Total'
+select t.title as 'Title',
+       count(*) as 'Total'
 from titles t
          join dept_emp de
               ON t.emp_no = de.emp_no
@@ -36,7 +31,8 @@ group by t.title
 order by count(*) desc;
 
 
-select d.dept_name, concat(e.first_name, ' ', e.last_name) as 'Department Manager', s.salary as 'Salary'
+select d.dept_name, concat(e.first_name, ' ',e.last_name) as 'Department Manager',
+       s.salary as 'Salary'
 from departments as d
     inner join dept_manager as dm
       on d.dept_no = dm.dept_no
@@ -45,3 +41,18 @@ from departments as d
     inner join employees as e
        on s.emp_no = e.emp_no
 where dm.to_date = '9999-01-01' and s.to_date = '9999-01-01';
+
+
+select concat(e.first_name, ' ', e.last_name) as 'Employee',
+       d.dept_name as 'Department',
+       concat(e2.first_name, ' ', e2.last_name) as 'Manager'
+from employees as e
+    inner join dept_emp as de
+        on e.emp_no = de.emp_no
+    inner join departments as d
+        on de.dept_no = d.dept_no
+    inner join dept_manager as dm
+        on d.dept_no = dm.dept_no
+    inner join employees as e2
+        on dm.emp_no = e2.emp_no
+where de.to_date = '9999-01-01' and dm.to_date = '9999-01-01';
