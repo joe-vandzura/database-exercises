052a8055f8eb6e053a95b3e605b51198f5157aaf
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-03 12:17:51 -0600

    finished part two of employee database exercise of joins exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,8 +1,16 @@
-SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS department_manager
-FROM departments as d
+select d.dept_name as department_name, CONCAT(e.first_name, ' ', e.last_name) as department_manager
+from departments as d
     inner join dept_manager as dm
 on d.dept_no = dm.dept_no
     inner join employees e
         on dm.emp_no = e.emp_no
 where dm.to_date = '9999-01-01';
+
+select d.dept_name as department_name, concat(e.first_name, ' ', e.last_name) as department_manager
+from departments as d
+    inner join dept_manager as dm
+on d.dept_no = dm.dept_no
+    inner join employees e
+on dm.emp_no = e.emp_no
+where e.gender = 'F' and dm.to_date = '9999-01-01';
 
