1c188fc1d75ff769d548966eff4cd867d7524776
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-03 14:31:12 -0600

    finished part three and four of employee database exercise of joins exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -14,3 +14,34 @@ on d.dept_no = dm.dept_no
 on dm.emp_no = e.emp_no
 where e.gender = 'F' and dm.to_date = '9999-01-01';
 
+select t.title, count(*) as total
+from titles as t
+inner join employees as e
+    on t.emp_no = e.emp_no
+inner join dept_emp as de
+    on e.emp_no = de.emp_no
+where de.dept_no = 'd009' and de.to_date = '9999-01-01'
+group by title;
+
+select t.title as 'title',
+       COUNT(*) as 'Total'
+from titles t
+         join dept_emp de
+              ON t.emp_no = de.emp_no
+         join departments d
+              on d.dept_no = de.dept_no
+  where year(t.to_date) = 9999
+  and d.dept_no = 'd009'
+group by t.title
+order by count(*) desc;
+
+
+select d.dept_name, concat(e.first_name, ' ', e.last_name) as 'Department Manager', s.salary as 'Salary'
+from departments as d
+    inner join dept_manager as dm
+      on d.dept_no = dm.dept_no
+    inner join salaries as s
+       on dm.emp_no = s.emp_no
+    inner join employees as e
+       on s.emp_no = e.emp_no
+where dm.to_date = '9999-01-01' and s.to_date = '9999-01-01';
